a0a86f99189d56efceb537ff7018ad852f57cbfb
NTMaia <natanmaia02@gmail.com>
2021-01-24 14:38:35 -0300

    Novos comandos para teste adicionados

--- logins/AreaDeComando.py
+++ logins/AreaDeComando.py
@@ -1,4 +1,14 @@
 from logins.comandos import pasta
 
 # Ao final do programa adicionar a opção de escolher a pasta.
-pasta.existePasta('/Users/Natan/Documents/drive/exception.txt')
+arquivo = '/Users/Natan/Documents/drive/exception.txt'
+pasta.ExistePasta(arquivo)
+pasta.Ver(arquivo)
+chave = str(input('Quer escrever um novo login? ')).upper().strip()
+if chave[0] == 'S':
+    site = str(input('Site: ')).strip()
+    login = str(input('Login: ')).strip()
+    senha = str(input('Senha: ')).strip()
+    pasta.Escrever(arquivo, site, login, senha)
+else:
+    pasta.Ver(arquivo)
