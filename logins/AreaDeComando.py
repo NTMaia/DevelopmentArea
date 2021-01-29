df39e835d09eab36b20ea37592f472006a7dc3c0
NTMaia <natanmaia02@gmail.com>
2021-01-29 15:14:02 -0300

    Finalização dos comandos necessarios para o funcionamento do programa

--- logins/AreaDeComando.py
+++ logins/AreaDeComando.py
@@ -1,5 +1,5 @@
 from logins.comandos import pasta, menu
 
 menu.Cabecalho('Informe a pasta a ser manipulada')
-diretorio = 'C:/Users/Natan/Documents/drive/exception.txt'  # str(input('Diretorio: '))
+diretorio = str(input('Diretorio: ')).strip()  # 'C:/Users/Natan/Documents/drive/exception.txt'
 menu.Menu(diretorio)
